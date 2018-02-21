8b68e35ad0a8a177b3bd6a7149fd829be6de890c
Viktor Luft <viktor.luft@freiheit.com>
2018-02-21 12:56:53 +0100

    Added coverage measurement

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './js/App';
 import registerServiceWorker from './registerServiceWorker';
 
 const root = document.getElementById('root');
